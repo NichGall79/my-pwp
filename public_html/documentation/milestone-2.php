c5897f8487b4d32f5ba41ebf9b9d9db40f7a0307
Nicholas Gallegos <ngallegos61@cnm.edugit config --global pull.rebase truegit config --global user.email ngallegos61@cnm.edu>
2020-04-02 06:19:19 -0600

    added doc files for persona

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title> Milestone 2 </title>
+</head>
+<body>
+	<h1><strong> Milestone 2 </strong></h1>
+	<p> My site will be one page and will consist of the following sections:</p>
+<div>
+	<h2><strong> Welcome: </strong></h2>
+	<p>An introduction to the page and a brief summary of the apps purpose  </p>
+</div>
+<div>
+	<h2><strong> Personal Objective: </strong></h2>
+	<p>An input of the users height and weight followed by their goal for using the app: to maintain or loose weight</p>
+</div>
+<div>
+	<h2><strong> Results: </strong></h2>
+	<p> A chart or graph followed by images displaying the progress made by former users before and after </p>
+</div>
+<div>
+	<h3><strong> Diet Tips/Community Comments: </strong></h3>
+	<p> A list of recipes voted on by users along with links to actual peer reviewed studies and a comments section </p>
+</div>
+<div>
+	<p><strong> Contact Information </strong> </p>
+	<li> Email and Support # </li>
+	<li> Linked Social Media, etc.. </li>
+</div>
+<h3> Mobile View </h3>
+<img src="mobileview.png" alt="mobilescreen image">
+<h3> Fullscreen View </h3>
+<img src="fullscreen.png" alt="fullscreen image">
+</body>
+
+
+
+
+
+
+
+
+</html>
