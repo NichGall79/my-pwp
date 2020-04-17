afa226727312861027b465a69689510cc0384e0e
Nicholas Gallegos <ngallegos61@cnm.edugit config --global pull.rebase truegit config --global user.email ngallegos61@cnm.edu>
2020-04-17 02:23:27 -0600

    made alterations to WireFrames

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -37,7 +37,7 @@
 	<li><strong> ATTITUDE: </strong> High energy </li>
 	<li><strong> BEHAVIOUR: </strong> Slightly impulsive </li>
 </ul>
-<p><strong> Usage: </strong></p>
+	<p><strong> Usage: </strong></p>
 <ul>
 	<li> Connect with other health nuts to exchange recipes and routines </li>
 	<li> Set new achievable fitness goals and track progress along the way </li>
@@ -47,5 +47,7 @@
 	<li> Seeking advice over anecdotal claims </li>
 	<li> Looking to add variety and mobility work to his routine </li>
 </div>
+	<p><strong> User Story :</strong></p>
+	<li> As a user John Doe will utilize the data from the website to make better health conscious decisions</li>
 </body>
 </html>
--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -8,30 +8,41 @@
 	<h1><strong> Milestone 2 </strong></h1>
 	<p> My site will be one page and will consist of the following sections:</p>
 <div>
-	<h2><strong> Welcome: </strong></h2>
-	<p>An introduction to the page and a brief summary of the apps purpose  </p>
+	<h2><strong> Welcome </strong></h2>
+	<p> An introduction to the page and a brief summary of the apps purpose </p>
 </div>
 <div>
 	<h2><strong> Personal Objective: </strong></h2>
-	<p>An input of the users height and weight followed by their goal for using the app: to maintain or loose weight</p>
+	<p> Goal for using the app: to maintain or loose weight</p>
 </div>
 <div>
-	<h2><strong> Results: </strong></h2>
-	<p> A chart or graph followed by images displaying the progress made by former users before and after </p>
+	<h2><strong> Cookbook </strong></h2>
+	<p> A list of simple recipes/food items along with links to actual peer reviewed studies explaining the benefits of certain ingredients </p>
 </div>
 <div>
-	<h3><strong> Diet Tips/Community Comments: </strong></h3>
-	<p> A list of recipes voted on by users along with links to actual peer reviewed studies and a comments section </p>
+	<h3><strong> Beginner Exercises </strong></h3>
+	<p> A compilation of images depicting body weight workouts </p>
 </div>
 <div>
-	<p><strong> Contact Information </strong> </p>
+	<h3><strong> The Importance of Stretching </strong></h3>
+	<p> A cited document explaining spinal alignment (erector spinae) and bracing the core </p>
+	<p> "Image of stretches here" </p>
+</div>
+<div>
+	<h3><strong> Intermediate Exercises </strong></h3>
+	<p> A compilation of images depicting a simple weight routine with compound exercises </p>
+</div>
+<div>
+	<p><strong> Contact Information </strong></p>
 	<li> Email and Support # </li>
 	<li> Linked Social Media, etc.. </li>
 </div>
+
+
 <h3> Mobile View </h3>
-<img src="mobileview.png" alt="mobilescreen image">
+<img src="PwpMobileView.png" alt="mobilescreen image">
 <h3> Fullscreen View </h3>
-<img src="fullscreen.png" alt="fullscreen image">
+<img src="PwpFullScreen.png" alt="fullscreen image">
 </body>
 
 
