5e73055dc557d2eec94cf57a47bb1690d4982166
Nicholas Gallegos <ngallegos61@cnm.edugit config --global pull.rebase truegit config --global user.email ngallegos61@cnm.edu>
2020-04-22 08:36:28 -0600

    added a lot of code will now commit more frequently

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -8,16 +8,16 @@
 	<h1><strong> Milestone 2 </strong></h1>
 	<p> My site will be one page and will consist of the following sections:</p>
 <div>
-	<h2><strong> Welcome </strong></h2>
-	<p> An introduction to the page and a brief summary of the apps purpose </p>
+	<h3><strong> Welcome </strong></h3>
+	<p> An introduction to the page and a brief summary of its purpose to educate users so that they might make informed decisions in their own life </p>
 </div>
 <div>
-	<h2><strong> Personal Objective: </strong></h2>
+	<h3><strong> Personal Objective: </strong></h3>
 	<p> Goal for using the app: to maintain or loose weight</p>
 </div>
 <div>
-	<h2><strong> Cookbook </strong></h2>
-	<p> A list of simple recipes/food items along with links to actual peer reviewed studies explaining the benefits of certain ingredients </p>
+	<h3><strong> Cookbook </strong></h3>
+	<p> A drop tab list of simple recipes/food items along with links listed below to actual peer reviewed studies explaining the benefits of certain ingredients </p>
 </div>
 <div>
 	<h3><strong> Beginner Exercises </strong></h3>
@@ -25,7 +25,7 @@
 </div>
 <div>
 	<h3><strong> The Importance of Stretching </strong></h3>
-	<p> A cited document explaining spinal alignment (erector spinae) and bracing the core </p>
+	<p> A cited document explaining spinal alignment regarding the erector spinae and bracing the core </p>
 	<p> "Image of stretches here" </p>
 </div>
 <div>
@@ -34,15 +34,17 @@
 </div>
 <div>
 	<p><strong> Contact Information </strong></p>
-	<li> Email and Support # </li>
+	<ul>
+	<li> Contact Form and Support # </li>
 	<li> Linked Social Media, etc.. </li>
+	</ul>
 </div>
 
 
-<h3> Mobile View </h3>
-<img src="PwpMobileView.png" alt="mobilescreen image">
-<h3> Fullscreen View </h3>
-<img src="PwpFullScreen.png" alt="fullscreen image">
+<h3><strong> Mobile View </strong></h3>
+<img src="PwpMobileView1.png" alt="mobilescreen image">
+	<h3><strong> Fullscreen View </strong></h3>
+<img src="PwpFullScreen1.png" alt="fullscreen image">
 </body>
 
 
